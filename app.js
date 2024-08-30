e025eeaa1721fb9d68a47e790d8842e0ada6b06e
AnukulPr1me <anukul_chandra@outlook.com>
2024-08-31 00:55:22 +0530

    establish socket connection

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const http = require("http");
-
-const PORT = process.env.PORT || 3000;
-
 const app = express();
 const server = http.createServer(app);
+const io = require("socket.io")(server);
+const PORT = process.env.PORT || 3001;
+
 app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
-})
+});
 
+let connectedPeers = [];
+ 
+
+io.on('connection', (socket) => {
+    connectedPeers.push(socket.id);
+    console.log(connectedPeers);
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+        const newConnectedPeers = connectedPeers.filter((peerSocketId) =>{
+            peerSocketId !== socket.io;
+        });
+        connectedPeers = newConnectedPeers;
+        console.log(connectedPeers);
+    });
+});
+ 
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
 });
