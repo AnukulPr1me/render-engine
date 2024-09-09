310f2d409a3ecbbf3a2251a863618a3f471d7fe2
AnukulPr1me <anukul_chandra@outlook.com>
2024-09-10 04:54:18 +0530

    finishing dialog call box

--- public/js/elements.js
+++ public/js/elements.js
@@ -6,6 +6,42 @@ export const getIncomingCallDialog = (callTypeInfo, acceptCallHandler, rejectCal
     dialogContent.classList.add("dialog_content");
     dialog.appendChild(dialogContent);
 
+    const title = document.createElement("p");
+    title.classList.add('dialog_title');
+    title.innerHTML = `Incoming ${callTypeInfo} call`;
+
+    const imageContainer = document.createElement("div");
+    imageContainer.classList.add("dialog_image_container");
+    const image = document.createElement("img");
+    const avtarImagePath = "./utils/images/dialogAvatar.png";
+    image.src = avtarImagePath;
+    imageContainer.appendChild(image);
+    
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("dialog_button_container")
+
+    const acceptCallButton = document.createElement("button");
+    acceptCallButton.classList.add("dialog_accept_call_button");
+    const acceptCallImg = document.createElement("img");
+    acceptCallButton.classList.add("dialog_button_image");
+    const acceptCallImagePath = "./utils/images/acceptCall.png";
+    acceptCallImg.src = acceptCallImagePath;
+    acceptCallButton.appendChild(acceptCallImg);
+    buttonContainer.appendChild(acceptCallButton);
+
+    const rejectCallButton = document.createElement("button");
+    rejectCallButton.classList.add("dialog_reject_call_button");
+    const rejectCallImg = document.createElement("img");
+    rejectCallButton.classList.add("dialog_button_image");
+    const rejectCallImagePath = "./utils/images/rejectCall.png";
+    rejectCallImg.src = rejectCallImagePath;
+    rejectCallButton.appendChild(rejectCallImg);
+    buttonContainer.appendChild(rejectCallButton);
+
+    dialogContent.appendChild(title);
+    dialogContent.appendChild(imageContainer);
+    dialogContent.appendChild(buttonContainer);
+
     const dialogHTML = document.getElementById("dialog");
     dialogHTML.appendChild(dialog);    
 
